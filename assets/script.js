b70afe09c10a045a208b39dcb38f54ff4418d98b
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-09-13 16:07:26 -0600

    meals api request

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+var userFormEl = document.getElementById("submit-btn");
+var recipeInputEl = document.getElementById("searchRecipe")
+
 // Slideshow for Pictures 
 var slideIndex = 0;
 showSlides();
@@ -13,3 +16,38 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 7000); // Change image every 5 seconds
 }
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+    // get value from input element & send it to getRecipes()
+    var searchTerm = recipeInputEl.value.trim();
+    
+    if (searchTerm) {
+        getRecipes(searchTerm);
+        // clear the input element
+        recipeInputEl.value = "";
+    } else {
+        alert("Please enter a valid search term.");
+    }
+};
+
+var getRecipes = function(searchTerm) {
+  // format the mealsdb api url
+  var apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchTerm;
+  
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(data) {
+        console.log(data, searchTerm);
+      })
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to Github");
+  });
+};
+
+userFormEl.addEventListener("click", formSubmitHandler);

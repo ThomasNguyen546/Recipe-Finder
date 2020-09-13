96f446634cdb05c3d16984fec9be2868e0be8b13
Thomas Nguyen <thomasnguyen546@gmail.com>
2020-09-13 00:16:30 -0600

    added fading carousel

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-console.log ("hello")
+// Slideshow for Pictures 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 5 seconds
+}
